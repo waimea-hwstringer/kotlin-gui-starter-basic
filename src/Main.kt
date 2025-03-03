c5d0df2bcbc726175fcc9f1d5165172af7816091
Harry Stringa <hwstringer@waimea.school.nz>
2025-03-04 09:59:54 +1300

    gronglr

--- src/Main.kt
+++ src/Main.kt
@@ -10,6 +10,7 @@
 
 import com.formdev.flatlaf.FlatDarkLaf
 import java.awt.*
+import java.awt.Color.*
 import java.awt.event.*
 import javax.swing.*
 
@@ -62,14 +63,20 @@ class MainWindow : JFrame(), ActionListener {
      * Populate the UI with UI controls
      */
     private fun addControls() {
+        val defaultFont = Font(Font.SANS_SERIF, Font.PLAIN, 36)
+
         greetingLabel = JLabel("Hello, World!")
         greetingLabel.horizontalAlignment = SwingConstants.CENTER
         greetingLabel.bounds = Rectangle(50, 50, 500, 100)
+        greetingLabel.font = defaultFont
         add(greetingLabel)
 
         helloButton = JButton("Click Me!")
         helloButton.bounds = Rectangle(50,200,500,100)
+        helloButton.foreground = Color.red
+        helloButton.background = Color(0,33,66)
         helloButton.addActionListener(this)     // Handle any clicks
+        helloButton.font = defaultFont
         add(helloButton)
     }
 
