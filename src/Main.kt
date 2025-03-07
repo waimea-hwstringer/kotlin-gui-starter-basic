d97de6c60ed0da3629c105ac412e1eb517186379
Harry Stringa <hwstringer@waimea.school.nz>
2025-03-07 14:41:27 +1300

    drew

--- src/Main.kt
+++ src/Main.kt
@@ -29,11 +29,13 @@ fun main() {
  * Defines the UI and responds to events
  * The app model should be passwd as an argument
  */
-class MainWindow : JFrame(), ActionListener {
+class MainWindow : JFrame(), ActionListener, KeyListener {
 
     // Fields to hold the UI elements
     private lateinit var greetingLabel: JLabel
+    private lateinit var textField: JTextField
     private lateinit var helloButton: JButton
+    private lateinit var goodbyeButton: JButton
 
     /**
      * Configure the UI and display it
@@ -51,7 +53,7 @@ class MainWindow : JFrame(), ActionListener {
      */
     private fun configureWindow() {
         title = "Kotlin Swing GUI Demo"
-        contentPane.preferredSize = Dimension(600, 350)
+        contentPane.preferredSize = Dimension(570, 440)
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         isResizable = false
         layout = null
@@ -65,19 +67,34 @@ class MainWindow : JFrame(), ActionListener {
     private fun addControls() {
         val defaultFont = Font(Font.SANS_SERIF, Font.PLAIN, 36)
 
-        greetingLabel = JLabel("Hello, World!")
+        greetingLabel = JLabel("Click a button")
         greetingLabel.horizontalAlignment = SwingConstants.CENTER
         greetingLabel.bounds = Rectangle(50, 50, 500, 100)
         greetingLabel.font = defaultFont
         add(greetingLabel)
 
-        helloButton = JButton("Click Me!")
-        helloButton.bounds = Rectangle(50,200,500,100)
-        helloButton.foreground = Color.red
+        textField = JTextField("Name...")
+        textField.horizontalAlignment = SwingConstants.CENTER
+        textField.addKeyListener(this)
+        textField.bounds = Rectangle(50, 150, 450, 100)
+        textField.font = defaultFont
+        add(textField)
+
+        helloButton = JButton("Hello!")
+        helloButton.bounds = Rectangle(50,300,200,100)
+        helloButton.foreground = Color.white
         helloButton.background = Color(0,33,66)
         helloButton.addActionListener(this)     // Handle any clicks
         helloButton.font = defaultFont
         add(helloButton)
+
+        goodbyeButton = JButton("Goodbye!")
+        goodbyeButton.bounds = Rectangle(300,300,200,100)
+        goodbyeButton.foreground = Color.white
+        goodbyeButton.background = Color(0,33,66)
+        goodbyeButton.addActionListener(this)     // Handle any clicks
+        goodbyeButton.font = defaultFont
+        add(goodbyeButton)
     }
 
 
@@ -86,11 +103,64 @@ class MainWindow : JFrame(), ActionListener {
      */
     override fun actionPerformed(e: ActionEvent?) {
         when (e?.source) {
+
+            textField -> {
+                println("pop")
+            }
+
             helloButton -> {
-                greetingLabel.text = "You clicked the button!"
+                val name = textField.text
+                if(name == "harry") {
+                    textField.text = "access granted"
+                    greetingLabel.foreground = Color.green
+                    greetingLabel.text = "welcome back harry!"
+                }
+                else if (name == "") {
+                    greetingLabel.text = "enter a name"
+                    greetingLabel.foreground = Color.orange
+                }
+                else {
+                    textField.text = "access denied"
+                    greetingLabel.foreground = Color.red
+                    greetingLabel.text = "user '${name}' not found!"
+                }
+            }
+
+            goodbyeButton -> {
+                val name = textField.text
+                if(name=="harry") {
+                    greetingLabel.text = "Goodbye, harry!!"
+                    greetingLabel.foreground = Color.blue
+                }
+                else if (name=="" || name=="Name..."){
+                    greetingLabel.text = "Goodbye, stranger."
+                    greetingLabel.foreground = Color.yellow
+                }
+                else {
+                    greetingLabel.text = "Goodbye, ${name}."
+                    greetingLabel.foreground = Color.yellow
+                }
             }
         }
     }
 
+    override fun keyTyped(e: KeyEvent?) {
+        println("key typed: ${e?.keyChar}")
+    }
+
+    override fun keyPressed(e: KeyEvent?) {
+        println("key pressed: ${e?.keyCode}")
+
+        if (e?.keyCode in KeyEvent.VK_A..KeyEvent.VK_Z) {
+            println("Letter key!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        }
+        else {
+            e?.consume()
+        }
+    }
+
+    override fun keyReleased(e: KeyEvent?) {
+        println("key released: ${e?.keyCode}")
+    }
 }
 
